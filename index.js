cba36ba6cec6206ab0d7cbb9df31dd83123418dd
Hyunah Park <hapark85@gmail.com>
2021-01-22 00:00:50 +0900

    middlewares

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 import express from "express";
 import morgan from "morgan";
+import helmet from "helmet";
 const app = express()
 const PORT = 4000;
 const handleListening = () => 
@@ -8,6 +9,9 @@ console.log(`Listening on: http://localhost:${PORT}`)
 const handleHome = (req, res) => res.send("what's up");
 
 const handleProfile = (req, res) => res.send("here's profile")
+
+app.use(morgan("dev"))
+app.use(helmet());
 
 app.get('/', handleHome);
 
