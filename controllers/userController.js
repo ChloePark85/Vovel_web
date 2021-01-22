0658bc082aa6a85aa06fe7846d15d0720cbe4ba6
Hyunah Park <hapark85@gmail.com>
2021-01-22 20:53:48 +0900

    added search controller

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,7 +1,7 @@
-export const join = (req, res) => res.render("join");
-export const login = (req, res) => res.render("login");
-export const logout = (req, res) => res.render("logout");
-export const users = (req, res) => res.render("users");
-export const userDetail = (req, res) => res.render("user Detail");
-export const editProfile = (req, res) => res.render("edit Profile");
-export const changePassword = (req, res) => res.render("change Password");
+export const join = (req, res) => res.render("join", { pageTitle: "회원가입"});
+export const login = (req, res) => res.render("login", { pageTitle: "로그인"});
+export const logout = (req, res) => res.render("logout", { pageTitle: "로그아웃"});
+export const users = (req, res) => res.render("users", { pageTitle: "사용자"});
+export const userDetail = (req, res) => res.render("user Detail", { pageTitle: "마이페이지"});
+export const editProfile = (req, res) => res.render("edit Profile", { pageTitle: "프로필편집"});
+export const changePassword = (req, res) => res.render("change Password", { pageTitle: "비밀번호변경"});
--- controllers/videoController.js
+++ controllers/videoController.js
@@ -1,7 +1,15 @@
-export const home = (req, res) => res.render("home");
-export const search = (req, res) => res.render("search")
-export const videos = (req, res) => res.render("videos")
-export const upload = (req, res) => res.render("upload")
-export const videoDetail = (req, res) => res.render("video Detail")
-export const editVideo = (req, res) => res.render("edit Video")
-export const deleteVideo = (req, res) => res.render("delete Video")
+export const home = (req, res) => res.render("home", { pageTitle : "홈" });
+
+export const search = (req, res) => {
+    const {query: { keyword : searchingBy }} = req;
+    res.render("search", { pageTitle : "검색", searchingBy: searchingBy})
+}
+export const videos = (req, res) => res.render("videos", { pageTitle : "보블목록"})
+
+export const upload = (req, res) => res.render("upload", { pageTitle : "업로드"})
+
+export const videoDetail = (req, res) => res.render("video Detail", { pageTitle : "콘텐츠소개"})
+
+export const editVideo = (req, res) => res.render("edit Video", { pageTitle : "보블편집"})
+
+export const deleteVideo = (req, res) => res.render("delete Video", { pageTitle : "보블삭제"})
